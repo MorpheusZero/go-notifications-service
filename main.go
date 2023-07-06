6c4d6e0b83cee457b1719659a5f52a8f3cd3b039
Dylan Legendre <dylanlegendre09@gmail.com>
2023-07-05 23:12:07 -0500

    few simple routes

--- main.go
+++ main.go
@@ -1,7 +1,45 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	mux := http.NewServeMux()
+	mux.HandleFunc("/health", GetHealthCheck)
+	mux.HandleFunc("/v1/notify", PostNotify)
+	err := http.ListenAndServe(":3000", mux)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func GetHealthCheck(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("content-type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("ok"))
+}
+
+func PostNotify(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("content-type", "application/json")
+
+	if r.Method == http.MethodPost {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("ok"))
+		var payload = &NotificationsPayload{NotificationType: "discord", Data: ""}
+		go NotificationsHandler(payload)
+	} else {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		w.Write([]byte("Method Not Allowed!"))
+	}
+}
+
+type NotificationsPayload struct {
+	NotificationType string `json:"notification_type"`
+	Data             string `json:"data"`
+}
+
+func NotificationsHandler(payload *NotificationsPayload) {
+	fmt.Println(payload.NotificationType)
 }
